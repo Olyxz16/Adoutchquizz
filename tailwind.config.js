4d4ffa6c28341d3d693a29cce49e4c11b0f80e39
Cédric Sazos <cedric.sazos@tutanota.com>
2024-09-30 15:14:44 +0200

    Added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.{templ}", "./static/css/main.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
