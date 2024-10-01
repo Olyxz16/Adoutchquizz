7a79134c5cd5a6b43d1f3bee797562834d5f5173
Cédric Sazos <cedric.sazos@tutanota.com>
2024-10-01 11:31:04 +0200

    Fixed invalid glob pattern

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.{templ}", "./static/css/main.css"],
+  content: ["./views/**/*.templ", "./static/css/main.css"],
   theme: {
     extend: {},
   },
